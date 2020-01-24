f9045eff3fb40cdd4697b36dd48057e9b2364722
schultzla <schultzlogana@gmail.com>
2020-01-24 02:47:54 -0500

    Changed validation to only happen on submit

--- client/src/components/Submission.js
+++ client/src/components/Submission.js
@@ -14,7 +14,7 @@ export default class Submission extends Component {
             messages: [],
             messageError: null,
             authorError: null,
-            error: true
+            error: false
         };
         
         this.getMessages = this.getMessages.bind(this);
@@ -31,6 +31,12 @@ export default class Submission extends Component {
                 authorError: author.length > 0 ? (author.length < 3 ? 'Author must be more than 3 characters' : null) : null
             })
         }
+
+        if (author.length === 0) {
+            return false;
+        } else {
+            return author.length < 3;
+        }
     }
 
     validateMessage = () => {
@@ -40,21 +46,28 @@ export default class Submission extends Component {
                 messageError: "Message is empty",
                 error: true
             })
+            return true;
         } else {
             this.setState({
                 messageError: message.length <= 0 ? (message.length > 140 ? 'Message must be less than 140 characters' : 'Message is empty') : null,
                 error: message.length <= 0 ? (message.length > 140 ? true: true) : false
             })
         }
+
+        if (message.length > 0 && message.length < 140) {
+            return false;
+        } else {
+            return true;
+        }
     }
 
     render() {
         return (
             <div className='container'>
-                <input type='text' ref={el => this.inputAuthor = el} onBlur={this.validateAuthor} onChange={this.updateAuthor} className={`border-0 bg-dark text-white form-control mb-2 ${this.state.authorError ? 'is-invalid' : ''}`} placeholder='Username (Optional)'></input>
+                <input type='text' ref={el => this.inputAuthor = el} onChange={this.updateAuthor} className={`border-0 bg-dark text-white form-control mb-2 ${this.state.authorError ? 'is-invalid' : ''}`} placeholder='Username (Optional)'></input>
                 <div className='invalid-feedback'>{this.state.authorError}</div>
 
-                <textarea ref={el => this.inputMessage = el} onBlur={this.validateMessage} onChange={this.updateMessage} onKeyDown={this.onEnter} className={`border-0 bg-dark text-white form-control ${this.state.messageError ? 'is-invalid' : ''}`} type='text' rows='5' resize='none' placeholder='Message'></textarea>
+                <textarea ref={el => this.inputMessage = el} onChange={this.updateMessage} onKeyDown={this.onEnter} className={`border-0 bg-dark text-white form-control ${this.state.messageError ? 'is-invalid' : ''}`} type='text' rows='5' resize='none' placeholder='Message'></textarea>
                 <div className='invalid-feedback'>{this.state.messageError}</div>
 
                 <button type="button" onMouseUp={this.addMessage} disabled={this.state.error} className="mt-2 btn btn-warning btn-block">Submit</button>
@@ -71,10 +84,12 @@ export default class Submission extends Component {
     }
 
     addMessage = event => {
-        if (this.state.authorError != null || this.state.messageError != null) {
+        var valMsg = this.validateMessage();
+        var valAuth = this.validateAuthor(); 
+        if (valMsg || valAuth) {
             return;
         }
-        this.capMessages();
+
         fetch('/api/v1/messages', {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
@@ -98,15 +113,19 @@ export default class Submission extends Component {
     }
 
     updateMessage = event => {
-        this.setState({ message: event.target.value }, () => {
-            this.validateMessage();
-          });
+        this.setState({ 
+            message: event.target.value,
+            error: false,
+            messageError: null
+        });
     }
 
     updateAuthor = event => {
-        this.setState({ author: event.target.value }, () => {
-            this.validateAuthor();
-          });
+        this.setState({ 
+            author: event.target.value,
+            error: false,
+            authorError: null
+         });
     }
 
     capMessages() {
