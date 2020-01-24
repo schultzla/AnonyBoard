9fe68931df424b829e0a6feb7d42df557a805ad6
schultzla <schultzlogana@gmail.com>
2020-01-23 22:47:10 -0500

    heroku stuff

--- server/app.js
+++ server/app.js
@@ -13,10 +13,14 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '../client/build')));
 
 app.use('/', indexRouter);
 app.use('/api/v1/messages', messageRouter);
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname + "../client/build/index.html"));
+    });
 
 const dbuser = 'anonyTweetAdmin'
 const dbpass = 'MelWykRqfkqFRSYZ'
